78b37a0949a65d278ed977b08ddfe2652fd79ac5
bijan102 <bfazeli1@gmail.com>
2017-07-27 23:05:42 -0700

    Update: Added additional methods to generate a random color.

--- ColorGame.js
+++ ColorGame.js
@@ -1,20 +1,14 @@
 /**
  * Created by bijanfazeli on 7/25/17.
  */
-var colors = [
-    "rgb(255, 0, 0)",
-    "rgb(255, 255, 0)",
-    "rgb(0, 255, 0)",
-    "rgb(0, 255, 255)",
-    "rgb(0, 0, 255)",
-    "rgb(255, 0, 255)",
-];
+var colors = generateRandomColors(6);
 
 // Hook up components
 var squares = document.querySelectorAll(".square");
 var pickedColor = pickColor();
 var colorDisplay = document.getElementById("colorDisplay");
 var messageDisplay = document.querySelector("#message");
+var h1 = document.querySelector("h1");
 
 colorDisplay.textContent = pickedColor;
 
@@ -27,9 +21,9 @@ for(var i = 0; i < squares.length; ++i) {
        // Grab color of clicked square and compare to pickedColor
         var clickedColor = this.style.backgroundColor;
         if (clickedColor === pickedColor) {
-            alert("Correct");
             messageDisplay.textContent = "Correct!";
             changeColors(clickedColor);
+            h1.style.backgroundColor = clickedColor;
         }
         else {
             this.style.backgroundColor = "#232323";
@@ -48,4 +42,30 @@ function changeColors (color) {
 
 function pickColor () {
     return colors[Math.floor(Math.random() * colors.length)];
+}
+
+function generateRandomColors (num) {
+    // Make an array
+    var arr = [];
+
+    // Add num random colors to arr
+    for(var i = 0; i < num; ++i)
+    {
+        // Get random color and push into arr
+        arr.push(randomColor());
+    }
+
+    // Return arr
+    return arr;
+}
+
+function randomColor () {
+    // Pick a 'red' from 0 - 255
+    var color = "rgb(" + Math.floor(Math.random() * 256) + ", ";
+    // Pick a 'green' from 0 - 255
+    color += Math.floor(Math.random() * 256) + ", ";
+    // Pick a 'blue' from 0 - 255
+    color += Math.floor(Math.random() * 256) + ")";
+
+    return color;
 }
